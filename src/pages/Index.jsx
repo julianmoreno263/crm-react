02d689d1ca78edbd4ae46ae40f04c872f9cc3266
Julián Moreno <julianmoreno263@gmail.com>
2022-10-28 18:13:01 -0500

    Primer commit app CRM React-Vite

---
+++ src/pages/Index.jsx
@@ -0,0 +1,10 @@
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Index-Clientes</h1>
+        </div>
+    )
+}
+
+export default Index
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,10 @@
+
+const NuevoCliente = () => {
+    return (
+        <div>
+            <h1>Nuevo Cliente</h1>
+        </div>
+    )
+}
+
+export default NuevoCliente
